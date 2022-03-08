382a767cce9694c4e5126b8870b6855742d1090e
ArunkumarC96 <bkup2akumarc12@gmail.com>
2022-03-08 14:52:39 +0530

    source file created

---
+++ bankManagementApp.c
@@ -0,0 +1,7 @@
+//this is the start of a bank management app that I found in codewithc website. this is for educational purpose only.
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+	printf("this is the main function\n");
+}
